51a1d8fd6e566e41967cc260b215bc512c5ce802
Shabnam <mirshokraie@gmail.com>
2014-09-16 20:29:44 -0700

    changing tool name and the title

--- oldPythia-icst15/r-scripts/barPlot_coverage.R
+++ oldPythia-icst15/r-scripts/barPlot_coverage.R
@@ -20,7 +20,7 @@ ylim = c(0, 100), ylab="Coverage (%)", xlab="Experimental Objects", col=colors)
 myaxis <- c(2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38)
 myaxislab <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)  
 
-labels <- c('JSAttestor', 'Artemis')
+labels <- c('JSeft', 'Artemis')
 #inset <- c(0.1, 0.1)
 axis(1, at = myaxis, labels = myaxislab, cex.axis = 1)
 legend("topright", labels, fill=colors, bty="n")
